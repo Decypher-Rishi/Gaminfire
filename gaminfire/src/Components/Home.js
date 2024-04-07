19fb6f01b73780b9f721118fe3765557cdf969ff
Decypher-Rishi <rish.coder@gmail.com>
2024-04-07 12:55:45 +0530

    Update Home.js again

--- gaminfire/src/Components/Home.js
+++ gaminfire/src/Components/Home.js
@@ -25,7 +25,7 @@ export default function Home(e) {
     const handleSubmit = async (e) => {
         e.preventDefault();
         console.log(user);
-        const response = await fetch(`http://localhost:5000/api/auth/register`, {
+        const response = await fetch(`http://gaminfire-backend.vercel.app/api/auth/register`, {
             method: "POST",
             headers: {
                 'Content-Type': "application/json"
